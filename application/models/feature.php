534845bf9bc67bac4f0c9f85ce130503c6cf43d2
siddharthkhialani <siddharthkhialani@gmail.com>
2013-10-11 02:11:50 -0400

    models
    feature.php
    restaurant.php
    restaurant_feature.php

---
+++ application/models/feature.php
@@ -0,0 +1,31 @@
+<?php
+class feature extends CI_Model {
+
+
+function getAll_feature($arrayList=NULL){
+	
+     if(!(is_null($arrayList)))
+     	$this->db->where_in('feature_id',$arrayList);
+
+    $query=$this->db->get('feature');
+	if($query->num_rows()>0)
+	foreach($query->result() as $row){
+	$data[]=$row;
+	}
+return $data;
+}
+
+
+function insert_feature($arrayList){
+	foreach($arrayList as $array){
+		$this->db->insert('feature',$array);
+		if($this->db->mysql_error())
+			echo('error');
+}
+	
+}
+
+
+
+}
+?>
---
+++ application/models/restaurant_feature_list.php
@@ -0,0 +1,26 @@
+<?php
+class restaurant_feature_list extends CI_MOdel{
+	function getAll_restaurant_feature_list($arrayList=NULL){
+	
+     if(!(is_null($arrayList))){
+     	foreach($arrayList as $array)
+     	$this->db->where('restaurant_id',$array['restaurant_id']);
+     	$this->db->where('city',$array['city']);
+     }
+    $query=$this->db->get('restaurant_feature_list');
+	if($query->num_rows()>0)
+	foreach($query->result() as $row){
+	$data[]=$row;
+	}
+return $data;
+}
+
+
+function insert_feature($arrayList){
+	foreach($arrayList as $array){
+		$this->db->insert('restaurant_feature_list',$array);
+}
+	
+}
+} 
+?>
