edf24f96dadbe89c3665a91428ad134333f95f4d
Siddharth Khialani <siddharthkhialani@gmail.com>
2013-12-05 02:03:09 -0500

    some changes to similarity

--- application/views/similar_restaurants_view.php
+++ application/views/similar_restaurants_view.php
@@ -1,26 +1,67 @@
 <script type="text/javascript">
 $(function(){
+	
+	var city='<?php echo $city?>';
 	<?php
+		$x=0;
 		foreach ($restaurants as $key => $value) {
 			# code...
 			?>
-			var d='<div><?php echo $key?></div>';
-			$('#similar_restaurants').append(d);
+
+			
+			var resId='<?php echo $key?>';
+			$.ajax({
+					url: '/CI_BigData/index.php/similarity_controller/getRestaurantName/'+city+'/'+resId,
+					success: function(html){
+					$('#similar_restaurants').append("<div class='restaurantCell'><div>"+html+"</div><div  id='<?php echo $x.$city; ?>'></div></div>");
+					},
+					error: function(a, b, c) {
+						alert(a+" "+b+" "+c);
+					}
+				});
+		
+			//$('#similar_restaurants').append(city);
 	<?php
-		}
-	?>
-});
-</script>
-<html>
-<body>
-<div id='similar_restaurants'>
-	<?php
+	$x++;
+	if($x==8){
+		
+		$y=0;
 		foreach ($restaurants as $key => $value) {
-	?>
-		<div><?php echo $key?></div>
-	<?php
+			# code...
+			?>	
+
+			$.ajax({
+					url: '/CI_BigData/index.php/machine/features/'+city+'/'+restId,
+					success: function(html){
+						//alert(html);
+						$('#<?php echo $y.$city;?>').append(html);
+					},
+					error: function(a, b, c) {
+						alert(a+" "+b+" "+c);
+					}
+				});
+
+			<?php
+			$y++;
+			if($y==8){
+				break;
+			}
+		}
+
+
+
+		break;
+		}
 	}
 	?>
+});
+
+
+
+</script>
+
+
+<div id='similar_restaurants'>
+
 </div>
-</body>
-</html>
+
