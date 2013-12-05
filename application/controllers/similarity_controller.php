3aeef0eabbfc56a5246e297e33babf5dc3d3d84f
Gaurav Komera <gaurav13989@gmail.com>
2013-12-05 15:48:32 -0300

    no message

--- application/controllers/similarity_controller.php
+++ application/controllers/similarity_controller.php
@@ -1,54 +1,66 @@
 <?php
 class similarity_controller extends CI_Controller{
-	
-	public function calculateSimilarRestaurants($cityS,$cityD,$resId){
-		$this->load->model('restaurant_feature_list');
-		$arrayList['city']=$cityS;
-		$arrayList['restaurant_id']=$resId;
-		$a=array($arrayList);
-		$data=$this->restaurant_feature_list->getAll_restaurant_feature_list($a);
-		
-		foreach($data as$r){
-			$sourcefeatures[]=$r->feature_id;
-		}
-		
-		$this->load->model('restaurant');
-		$restaurants=$this->restaurant->getCityRestaurant($cityD);
 
-		foreach ($restaurants as $row) {
-			$id=$row->restaurant_id;
-			$array['city']=$cityD;
-			$array['restaurant_id']=$id;
-			$b=array($array);
-			$d=$this->restaurant_feature_list->getAll_restaurant_feature_list($b);
+public function calculateSimilarRestaurants($cityS,$cityD,$resId){
+$this->load->model('restaurant_feature_list');
+$arrayList['city']=$cityS;
+$arrayList['restaurant_id']=$resId;
+$a=array($arrayList);
+$data=$this->restaurant_feature_list->getAll_restaurant_feature_list($a);
 
-			foreach($d as $r){
-				$destinationfeatures[]=$r->feature_id;
-			}
-			
-			 $count=sizeof(array_intersect($sourcefeatures, $destinationfeatures));
-			$restaurant[$id]=$count;
-			
+foreach($data as$r){
+$sourcefeatures[]=$r->feature_id;
+}
 
-			//$all_restaurants[]=$restaurant;
+$this->load->model('restaurant');
+$restaurants=$this->restaurant->getCityRestaurant($cityD);
 
-		}
+foreach ($restaurants as $row) {
+$id=$row->restaurant_id;
+$array['city']=$cityD;
+$array['restaurant_id']=$id;
+$b=array($array);
+$d=$this->restaurant_feature_list->getAll_restaurant_feature_list($b);
 
-		arsort($restaurant);
+foreach($d as $r){
+$destinationfeatures[]=$r->feature_id;
+}
 
-		$send_to_view['restaurants']=$restaurant;
-		$send_to_view['city']=$cityD;
+$count=sizeof(array_intersect($sourcefeatures, $destinationfeatures));
+$restaurant[$id]=$count;
 
-		$this->load->view("similar_restaurants_view",$send_to_view);
+
+//$all_restaurants[]=$restaurant;
+
+}
+
+arsort($restaurant);
+foreach ($variable as $key => $value) {
+# code...
+$restaurant=$this->restaurant->getRestaurant($cityD,$key);
+$resName='';
+foreach ($restaurant as $r) {
+$resName=$r->restaurant_name;
+}
+$restaurant_names[$key]=$resName;
+}
+
+$this->load->model('uploadedfile');
+$cityName=$this->uploadedfile->getCityName($cityD);
+$send_to_view['restaurants']=$restaurant;
+$send_to_view['city']=$cityD;
+$send_to_view['city_name']=$cityName;
+$send_To_view['restaurant_names']=$restaurant_names;
+$this->load->view("similar_restaurants_view",$send_to_view);
 }
 
 public function getRestaurantName($city,$resId){
-	$this->load->model('restaurant');
-	$restaurant=$this->restaurant->getRestaurant($city,$resId);
-	$resName='';
-	foreach ($restaurant as $r) {
-		$resName=$r->restaurant_name;
-	}
-	echo $resName;
+$this->load->model('restaurant');
+$restaurant=$this->restaurant->getRestaurant($city,$resId);
+$resName='';
+foreach ($restaurant as $r) {
+$resName=$r->restaurant_name;
+}
+echo $resName;
 }
 }
